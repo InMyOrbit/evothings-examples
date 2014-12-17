8e4aea4cc90e3b5f0bc4626ebaa2f63e58b2305f
Andreas Lundquist <lundquist.andreas@gmail.com>
2014-12-17 17:39:36 +0100

    - Solved mediatek-linkit issue with Google Maps JavaScript API

--- examples/mediatek-linkit/app.js
+++ examples/mediatek-linkit/app.js
@@ -15,69 +15,72 @@
 //
 //
 
-document.addEventListener('deviceready',function(){ app.initialize() }, false);
-
 var app = {}
-
 app.TIME_BETWEEN_REQUESTS = 5000
 
-app.initialize = function()
+app.initializeMap = function initialize() 
 {
 
 	var mapOptions = {
-		center: { lat: 59.3380131, lng: 18.063002200000028},
+		center: { lat: 59.3383, lng: 18.0621},
     	zoom: 13,
     	
     	draggable: true,
     	panControl: false,
     	mapTypeControl: false,
     	streetViewControl: false,
-    	zoomControl:false,
-    	
+    	zoomControl:false,	
 	}
 	
 	app.map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions)
-	
 }
+
+google.maps.event.addDomListener(window, 'load', app.initializeMap);
 
 app.connect = function() 
 {
-
-	var IPAdress = $('#IPAdress').val()
+	app.IPAdress = $('#IPAdress').val()
 
 	$('#startView').hide()
 	
-	$('#connectingStatus').text('Connecting to ' + IPAdress)
+	$('#connectingStatus').text('Connecting to ' + app.IPAdress)
 	$('#connectingView').show()
-	
-	console.log('Trying to connect to ' + IPAdress)
 
-	app.dataFetcher = setInterval(function() { app.fetchData(); }, app.TIME_BETWEEN_REQUESTS);
+	console.log('Trying to connect to ' + app.IPAdress)
+
+	app.fetchTimer = setInterval(function() { app.fetchData(); }, app.TIME_BETWEEN_REQUESTS);
 }
 
 app.fetchData = function()
 {	
 	console.log('Trying to fetch data...')
 
-	$.getJSON('http://192.168.20.110', app.dataReceived)	
-
-	//$.getJSON('http://echo.jsontest.com/lat/59.3380131/long/18.063002200000028', app.dataReceived)	
+	$.getJSON('http://' + app.IPAdress, app.dataReceived)	
 }
 
 app.dataReceived = function(data, textStatus, xhr)
 {
 	$('#connectingView').hide()
 	$('#mapView').show()
+	google.maps.event.trigger(app.map, "resize")
+
 
 	var longitude = parseFloat(data['long'])
+	var longitudeDecimalPart = longitude % 1
+	longitude = longitude - longitudeDecimalPart + (longitudeDecimalPart * 100 / 60)
+
 	var latitude = parseFloat(data['lat']) 
+	var latitudeDecimalPart = latitude % 1
+	latitude = latitude - latitudeDecimalPart + (latitudeDecimalPart * 100 / 60)
+
 	var positionString = 'Latitude: ' + latitude + ', Longitude: ' + longitude
 	
 	console.log('Received data - ' + positionString)
 	
-	//app.marker.setMap(null)
-
+	if(app.marker) {
+		app.marker.setMap(null)
+	}
 
 	var markerPosition = new google.maps.LatLng(latitude, longitude)
 
@@ -89,267 +92,12 @@ app.dataReceived = function(data, textStatus, xhr)
 	app.marker.setMap(app.map)
 
 	app.map.panTo(app.marker.getPosition())
-
-	clearInterVal(app.dataFetcher)
-	
 }
 
 app.disconnect = function()
 {
-	$('#dataView').hide()
+	clearInterval(app.fetchTimer)
+
+	$('#mapView').hide()
 	$('#startView').show()
 }
-
-app.startScan = function()
-{
-	app.disconnect();
-
-	console.log('Scanning started...');
-
-	app.devices = {};
-
-	var htmlString = '<img src="img/loader_small.gif" style="display:inline; vertical-align:middle">' +
-					'<p style="display:inline">   Scanning...</p>';
-
-	$("#scanResultView").append($(htmlString));
-
-	$("#scanResultView").show();
-
-	function onScanSuccess(device)
-	{
-		if (device.name != null)
-		{
-			app.devices[device.address] = device; 
-
-			console.log('Found: ' + device.name + ', ' + device.address +
-				', ' + device.rssi);
-
-			var htmlString = '<div class="deviceContainer" onclick="app.connectTo(\'' + device.address + '\')">' +
-								'<p class="deviceName">' + device.name + '</p>' +
-								'<p class="deviceAddress">' + device.address + '</p>' +
-							'</div>';
-
-			$( "#scanResultView" ).append( $( htmlString ) );
-		}
-	};
-
-	function onScanFailure(errorCode) {
-		// Show an error message to the user
-		app.disconnect('Failed to scan for devices.');
-
-		// Write debug information to console.
-		console.log('Error ' + errorCode);
-	};
-
-	evothings.easyble.reportDeviceOnce(true);
-	evothings.easyble.startScan(onScanSuccess, onScanFailure); 
-
-	$( "#startView" ).hide();
-};
-
-app.setLoadingLabel = function(message)
-{
-	console.log(message);
-	$( '#loadingStatus').text(message);
-}
-
-app.connectTo = function(address)
-{
-
-	device = app.devices[address];
-
-	$( "#loadingView" ).css('display', 'table');
-
-	app.setLoadingLabel('Trying to connect to ' + device.name);
-
-	function onConnectSuccess(device)
-	{
-
-		function onServiceSuccess(device)
-		{
-			// Application is now connected
-			app.connected = true;
-			app.device = device;
-
-			console.log('Connected to ' + device.name);
-
-			device.writeDescriptor(
-				app.RBL_CHAR_TX_UUID, 
-				app.RBL_TX_UUID_DESCRIPTOR,
-				new Uint8Array([1,0]),
-				function()
-				{
-					console.log('Status: writeDescriptor ok.');
-
-					$( "#loadingView" ).hide();
-					$( "#scanResultView" ).hide();
-					$( "#controlView" ).show();
-				},
-				function(errorCode)
-				{
-					// Disconnect and give user feedback. 
-					app.disconnect('Failed to set descriptor.');
-
-					// Write debug information to console.
-					console.log('Error: writeDescriptor: ' + errorCode + '.');
-				}
-			);
-
-			function failedToEnableNotification(erroCode) {
-				console.log('BLE enableNotification error: ' + errorCode);
-			};
-
-			device.enableNotification(
-				app.RBL_CHAR_TX_UUID,
-				app.receivedData,
-				function(errorcode)
-				{
-					console.log('BLE enableNotification error: ' + errorCode);
-				}
-			);
-
-		};
-
-		function onServiceFailure(errorCode)
-		{
-			// Disconnect and show an error message to the user.
-			app.disconnect('Device is not from RedBearLab');
-
-			// Write debug information to console.
-			console.log('Error reading services: ' + errorCode);
-		};
-
-		app.setLoadingLabel('Identifying services...');
-
-		// Connect to the appropriate BLE service
-		device.readServices(
-			[app.RBL_SERVICE_UUID],
-			onServiceSuccess,
-			onServiceFailure
-		);
-	};
-
-	function onConnectFailure(errorCode)
-	{
-		// Disconnect and show an error message to the user.
-		app.disconnect('Disconnected from device');
-
-		// Write debug information to console
-		console.log('Error ' + errorCode);
-	};
-
-	// Stop scanning
-	evothings.easyble.stopScan();
-
-	// Connect to our device
-	console.log('Identifying service for communication');
-	device.connect(onConnectSuccess, onConnectFailure);
-};
-
-app.sendData = function(data)
-{
-	if (app.connected)
-	{
-
-		function onMessageSendSucces()
-		{
-			console.log('Succeded to send message.');
-		}
-
-		function onMessageSendFailure(errorCode)
-		{
-			console.log('Failed to send data with error: ' + errorCode);
-			app.disconnect('Failed to send data');
-		};
-
-		data = new Uint8Array(data);
-
-		app.device.writeCharacteristic(
-			app.RBL_CHAR_RX_UUID,
-			data,
-			onMessageSendSucces,
-			onMessageSendFailure
-		);
-	}
-	else
-	{
-		// Disconnect and show an error message to the user.
-		app.disconnect('Disconnected');
-
-		// Write debug information to console
-		console.log('Error - No device connected.');
-	}
-};
-
-app.receivedData = function(data)
-{
-	if (app.connected)
-	{
-		var data = new Uint8Array(data);
-
-		if (data[0] === 0x0A)
-		{
-			$( '#digitalInputResult').text(data[1] ? 'High' : 'Low');
-		}
-		else if (data[0] === 0x0B)
-		{
-			if (analog_enabled)
-			{
-				var number = (data[1] << 8) | data[2];
-				$( '#analogDigitalResult').text(number);
-			}
-		};
-
-		console.log('Data received: [' + data[0] +', ' + data[1] + ', ' + data[2] + ']');
-	}
-	else
-	{
-		// Disconnect and show an error message to the user.
-		app.disconnect('Disconnected');
-
-		// Write debug information to console
-		console.log('Error - No device connected.');
-	}
-};
-
-app.disconnect = function(errorMessage)
-{
-	if (errorMessage)
-	{
-		navigator.notification.alert(errorMessage,function alertDismissed() {});
-	}
-
-	app.connected = false;
-	app.device = null;
-
-	// Stop any ongoing scan and close devices.
-	evothings.easyble.stopScan();
-	evothings.easyble.closeConnectedDevices();
-
-	console.log('Disconnected');
-
-	$( "#loadingView" ).hide();
-	$( "#scanResultView" ).hide();
-	$( "#scanResultView" ).empty();
-	$( "#controlView" ).hide();
-	$( "#startView" ).show();
-};
-
-app.toggelAnalog =  function()
-{
-	if (analog_enabled)
-	{
-		analog_enabled = false;
-		app.sendData([0xA0,0x00,0x00]);
-		$( '#analogDigitalResult').text('-');
-		$( '#analogToggleButton').text('Enable Analog');
-		$( '#analogToggleButton').removeClass('blue wide').addClass('green wide');
-	}
-	else
-	{
-		analog_enabled = true;
-		app.sendData([0xA0,0x01,0x00]);
-		$( '#analogToggleButton').text('Disable Analog');
-		$( '#analogToggleButton').removeClass('green wide').addClass('blue wide');
-	}
-};
